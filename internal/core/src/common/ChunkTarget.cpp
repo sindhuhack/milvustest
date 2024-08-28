a2c2932a6dbf3d188bcdff6039d698613bb1974d
sunby <sunbingyi1992@gmail.com>
2024-08-28 15:56:29 +0800

    feat: support chunked column for sealed segment
    Signed-off-by: sunby <sunbingyi1992@gmail.com>

--- internal/core/src/common/ChunkTarget.cpp
+++ internal/core/src/common/ChunkTarget.cpp
@@ -10,9 +10,11 @@
 // or implied. See the License for the specific language governing permissions and limitations under the License
 
 #include <common/ChunkTarget.h>
+#include <algorithm>
 #include <cstring>
 #include "common/EasyAssert.h"
 #include <sys/mman.h>
+#include <unistd.h>
 
 namespace milvus {
 void
@@ -62,8 +64,21 @@ MmapChunkTarget::seek(size_t offset) {
 
 std::pair<char*, size_t>
 MmapChunkTarget::get() {
+    // Write padding to align with the page size, ensuring the offset_ aligns with the page size.
+    auto page_size = sysconf(_SC_PAGE_SIZE);
+    auto padding_size =
+        (size_ / page_size + (size_ % page_size != 0)) * page_size - size_;
+    char padding[padding_size];
+    memset(padding, 0, sizeof(padding));
+    write(padding, padding_size);
+
     auto m = mmap(
         nullptr, size_, PROT_READ, MAP_SHARED, file_.Descriptor(), offset_);
+    AssertInfo(m != MAP_FAILED,
+               "failed to map: {}, map_size={}, offset={}",
+               strerror(errno),
+               size_,
+               offset_);
     return {(char*)m, size_};
 }
 
