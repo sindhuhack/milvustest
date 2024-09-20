96566899dc44bfac1681512b2070afe6b0f9d564
SimFG <bang.fu@zilliz.com>
2024-09-20 17:48:24 +0800

    fix: the compile error and fit the index file size attribute

--- internal/querynodev2/segments/segment.go
+++ internal/querynodev2/segments/segment.go
@@ -1118,6 +1118,7 @@ func GetCLoadInfoWithFunc(ctx context.Context,
 		IndexFiles:         indexInfo.GetIndexFilePaths(),
 		IndexEngineVersion: indexInfo.GetCurrentIndexVersion(),
 		IndexStoreVersion:  indexInfo.GetIndexStoreVersion(),
+		IndexFileSize:      indexInfo.GetIndexSize(),
 	}
 
 	// 2.
